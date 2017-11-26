42a486548589c596f8fff8d90f3ddc781e68c342
cagataycali <cagataycali@icloud.com>
2017-11-26 14:51:48 +0300

    ✨  Step 3 - Set properties on react components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 class App extends React.Component {
   render () {
-    return (
-      <div>
-        <h1>Hello world</h1>
-        <b>Bold one!</b>
-      </div>
-    )
+    return <h1>{this.props.txt}</h1>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
-  <App />,
+  <App txt='this is the prop text' />,
   document.getElementById('root')
 )
