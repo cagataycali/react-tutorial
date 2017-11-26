ccd1d9d98e9e97782d07ac26aeccb49be66da68d
cagataycali <cagataycali@icloud.com>
2017-11-26 14:55:39 +0300

    ✨  Step 4 - PropType checking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class App extends React.Component {
   render () {
     return <h1>{this.props.txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: PropTypes.string
 }
 
 export default App
