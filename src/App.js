6c9679cd6b89059da181296a7deef39a1d63b01c
cagataycali <cagataycali@icloud.com>
2017-11-26 14:47:49 +0300

    ✨  Step 2 - Display outputs with render

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 class App extends React.Component {
   render () {
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <b>Bold one!</b>
+      </div>
+    )
   }
 }
 
